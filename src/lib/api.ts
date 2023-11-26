820c6a6512249d09f4b3d9462e67188f37b0c47f
Łukasz Warchoł <lukaszwarchol.dev@gmail.com>
2023-11-26 11:06:38 +0100

    added post requests

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,5 +1,10 @@
 import { env } from '@/env.mjs';
 
+export type Post = {
+  slug: string;
+  title: string;
+};
+
 async function fetchGraphQL(query: string) {
   return fetch(`https://graphql.contentful.com/content/v1/spaces/${env.CONTENTFUL_SPACE_ID}`, {
     method: 'post',
@@ -25,4 +30,48 @@ export async function getHomePageContent(locale: string) {
   );
 
   return response?.data?.homePageCollection?.items[0];
+}
+
+export async function getBlogContent(locale: string) {
+  const response = await fetchGraphQL(
+    `query {
+      blogCollection(locale: "${locale}", limit: 1) {
+        items {
+          heading
+        }
+      }
+    }`,
+  );
+
+  return response?.data?.blogCollection?.items[0];
+}
+
+export async function getAllPosts(locale: string): Promise<Post[]> {
+  const response = await fetchGraphQL(
+    `query {
+      postCollection(where: { slug_exists: true }, locale: "${locale}", order: date_DESC) {
+        items {
+          slug
+          title
+        }
+      }
+    }`,
+  );
+
+  return response?.data?.postCollection?.items;
+}
+
+export async function getPost(locale: string, slug: string): Promise<Post> {
+  const response = await fetchGraphQL(
+    `query {
+      postCollection(where: { slug: "${slug}" }, locale: "${locale}", limit: 1) {
+        items {
+          slug
+          title
+        }
+      }
+    }`,
+  );
+
+  return response?.data?.postCollection?.items[0];
 }
