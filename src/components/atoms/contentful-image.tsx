18900e37557e461790fbfd8ecd670436cbbfb7fa
Łukasz Warchoł <lukaszwarchol.dev@gmail.com>
2023-11-29 19:04:59 +0100

    added contentful image

---
+++ src/components/atoms/contentful-image.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+import Image from 'next/image';
+
+interface ContentfulLoaderProps {
+  src: string;
+  width?: number;
+  quality?: number;
+}
+
+interface ContentfulImageProps extends ContentfulLoaderProps {
+  height?: number;
+  alt: string;
+  priority?: boolean;
+}
+
+const contentfulLoader = ({ src, width, quality }: ContentfulLoaderProps) => {
+  return `${src}?w=${width}&q=${quality || 75}`;
+};
+
+export function ContentfulImage(props: ContentfulImageProps) {
+  return <Image loader={contentfulLoader} {...props} />;
+}
