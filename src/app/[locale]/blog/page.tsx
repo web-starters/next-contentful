ad0b2d6b66fcdf6709a29aacf6e81aaaf96013f2
Łukasz Warchoł <lukaszwarchol.dev@gmail.com>
2023-11-29 19:35:18 +0100

    added post list

--- src/app/[locale]/blog/page.tsx
+++ src/app/[locale]/blog/page.tsx
@@ -1,7 +1,10 @@
-import { type Locale } from '@/i18n-config';
-import { getBlogContent } from '@/lib/api';
+import Link from 'next/link';
 
-import { Heading } from '@/components/atoms/heading';
+import { type Locale } from '@/i18n-config';
+import { getAllPosts, getBlogContent } from '@/lib/api';
+
+import { Heading, HeadingTypeEnum } from '@/components/atoms/heading';
+import { ContentfulImage } from '@/components/atoms/contentful-image';
 
 interface Props {
   params: { locale: Locale };
@@ -9,11 +12,33 @@ interface Props {
 
 export default async function Page({ params }: Props) {
   const content = await getBlogContent(params.locale);
+  const allPosts = await getAllPosts(params.locale);
 
   return (
     <section className="w-full p-5">
       <div className="w-full max-w-7xl mx-auto">
         <Heading>{content.heading}</Heading>
+
+        <div className="grid grid-cols-3 gap-5">
+          {allPosts.map(post => (
+            <Link key={post.slug} href={`/blog/${post.slug}`}>
+              <div>
+                <ContentfulImage
+                  className="mb-2 rounded-md"
+                  src={post.coverImage.url}
+                  width={400}
+                  height={200}
+                  alt={post.title}
+                  priority
+                />
+
+                <Heading type={HeadingTypeEnum.H4} size="xs">
+                  {post.title}
+                </Heading>
+              </div>
+            </Link>
+          ))}
+        </div>
       </div>
     </section>
   );
